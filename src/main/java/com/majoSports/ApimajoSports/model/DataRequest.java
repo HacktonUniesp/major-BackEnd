a05a6d14d5cd0dad3f11597c9cf6561a1048bbbd
felipe <felipejiti@hotmail.com>
2024-06-29 12:39:06 -0300

    implementado a entidade dataRequest

---
+++ src/main/java/com/majoSports/ApimajoSports/model/DataRequest.java
@@ -0,0 +1,21 @@
+package com.majoSports.ApimajoSports.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class DataRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String dateResponse;
+
+    private LocalDateTime dataReiceved;
+}
